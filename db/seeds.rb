b1a70517a383a9ebd5438b401f6ebaa656eb2375
Milo Wissig <mwissig@gmail.com>
2018-09-01 12:32:16 -0400

    seeds for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
+50.times do
+  i = rand(0..3)
+  edu = ["HS", "College", "Masters", "PHD"]
+  User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.unique.email,
+    age: rand(16..85),
+    education: edu[i],
+    salary: rand(36..120)*1000,
+    admin_priv: rand(0..1),
+    password: "password"
+  )
+end
 
 100.times do
   i = rand(0..3)
@@ -17,8 +30,49 @@
     email: Faker::Internet.unique.email,
     age: rand(16..85),
     education: edu[i],
-    grade: rand(0.90..4.00).ceil(2),
+    grade: rand(0.90..4.00).round(2),
     cohort_id: 0,
     course_id: rand(1..10)
   )
 end
+
+Course.create(
+  name: "Industrial Design",
+  hours: 220
+)
+Course.create(
+  name: "Textile Design",
+  hours: 180
+)
+Course.create(
+  name: "Environmental Policy",
+  hours: 150
+)
+Course.create(
+  name: "Computational Statistics",
+  hours: 280
+)
+Course.create(
+  name: "Electrical Engineering",
+  hours: 280
+)
+Course.create(
+  name: "Geometry And Topology",
+  hours: 200
+)
+Course.create(
+  name: "Ornithology",
+  hours: 190
+)
+Course.create(
+  name: "Clinical Microbiology",
+  hours: 260
+)
+Course.create(
+  name: "Computational Physics",
+  hours: 300
+)
+Course.create(
+  name: "Chamber Music",
+  hours: 190
+)
